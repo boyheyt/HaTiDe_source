d9c391c2287d1a40cb2a0fce627df4a6d8e5688b
boyheyt <59130357+boyheyt@users.noreply.github.com>
2020-04-02 18:26:11 +0700

    1.0
    version store and return if reset program

--- ManagermentJob.py
+++ ManagermentJob.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import tkinter as Functk
-import time as FuncTime
+import datetime as FuncTime
 
 global mIsTurnCount
 global mTotalSecondTime
@@ -9,6 +9,8 @@ global mTotalHourTime
 mTotalSecondTime = 0
 mTotalMinutesTime = 0
 mTotalHourTime = 0
+
+#display_time
 def display_time():
     global mTotalSecondTime
     global mTotalMinutesTime
@@ -36,13 +38,72 @@ def display_time():
     ClockLable['text'] = message
     top.after(1000,display_time)
 
-if __name__ == '__main__':
+#Handle exit
+#prameter isExit: False file have not current days
+#                 True the file have current days
+#        lineExit: Line date exits in system file.
+#
+def saveCounterFunc(isExit,lineExit):
+    #print("Exit program")
+    global mTotalSecondTime
+    global mTotalMinutesTime
+    global mTotalHourTime
+    totalTimeHour = mTotalHourTime + float(mTotalMinutesTime/60)
+    dateTimeNow =  FuncTime.datetime.today()
+    message = str(dateTimeNow.year)+"-"+str(dateTimeNow.month)+"-"+str(dateTimeNow.day) + ":" \
+    + str(mTotalHourTime)+"-"+str(mTotalMinutesTime)+"-"+str(mTotalSecondTime)+"\n"
+    if isExit != True:
+        infile  = open("system.txt","a+")
+        infile.writable
+        infile.write(str(message))
+        infile.closed
+    else:
+        infile  = open("system.txt","r")
+        listDataOfFile = infile.readlines()
+        infile.closed
+        listDataOfFile[lineExit] = message
+        outFile  = open("system.txt","w+")
+        outFile.writelines(listDataOfFile)
+        outFile.closed
+        
+#Func check same day
+def checkSameDayCounterFunc():
+    #print("Exit program")
+    global mTotalSecondTime
+    global mTotalMinutesTime
+    global mTotalHourTime
+    #get current day
+    dateTimeNow =  FuncTime.datetime.today()
+    currentday = str(dateTimeNow.year)+"-"+str(dateTimeNow.month)+"-"+str(dateTimeNow.day)
+    #get file system
+    infile  = open("system.txt","r")
+    contentTimeFile = infile.readlines()
+    infile.closed
+    countLine = 0
+    for index in contentTimeFile:
+        #extract the date
+        wordlist = index.split(":")
+        #judment
+        if wordlist[0] == currentday: # correct day
+            timeList = wordlist[1].split("-")
+            mTotalSecondTime = int(timeList[2])
+            mTotalMinutesTime = int(timeList[1])
+            mTotalHourTime = int(timeList[0])
+            return [True,countLine]
+        else :
+            countLine += 1
+    return [False,0]
 
+if __name__ == '__main__':
     top = Functk.Tk("Managerment Job")
     top.geometry("370x120")
     top.title("Time Task Counting")
     #Functk.Label(top, text="Main Name").grid(row=0)
     ClockLable = Functk.Label(top,font='ariel 80',bg="black",fg="red")
     ClockLable.grid(row=0,column=0)
+    #check time have saved or not
+    listDayExit = checkSameDayCounterFunc()
     display_time()
     top.mainloop()
+    #save time
+    saveCounterFunc(listDayExit[0],listDayExit[1])
